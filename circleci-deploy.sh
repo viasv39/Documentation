12f3612c37b3d2fb1c72b63564aa3064bbc4eeea
Daniel <danielmj@me.com>
2016-03-10 13:08:36 -0800

    simple docker-compose fixes

--- circleci-deploy.sh
+++ circleci-deploy.sh
@@ -2,12 +2,12 @@
 
 echo " "
 echo "Pulling Latest Changes"
-ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/CSC190; git reset --hard HEAD; git pull --rebase origin master"
+ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/csc190/main; git reset --hard HEAD; git pull --rebase origin master"
 
 echo " "
 echo "Docker-compose pulling"
-ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/CSC190/source/backend; docker-compose pull"
+ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/csc190/main/source/backend; docker-compose pull"
 
 echo " "
 echo "Docker-compose running"
-ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/CSC190/source/backend; docker-compose build; MYSQLPASS=$DEPLOY_MYSQL_PASS docker-compose up -d"
+ssh $DEPLOY_USER@$DEPLOY_HOST "cd ~/Class/csc190/main/source/backend; docker-compose build; MYSQLPASS=$DEPLOY_MYSQL_PASS docker-compose up -d"
